40ce542b07a0677dd199f27ebea478f2f2f6c914
Shashank Aditya <msrsaditya@gmail.com>
2025-05-17 21:40:19 +0530

    Update DistroPopularity.sh

--- DistroPopularity.sh
+++ DistroPopularity.sh
@@ -87,7 +87,21 @@ printf "\n"
 printf "Fetched "
 
 for distro in "${distros[@]}"; do
-    curl -s -o "$distro.html" "https://www.reddit.com/r/$distro/?rdt=11111"
+    curl -s -o "$distro.html" "https://www.reddit.com/r/$distro/" \
+        -H 'authority: www.reddit.com' \
+        -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+        -H 'accept-language: en-US,en;q=0.9' \
+        -H 'cache-control: max-age=0' \
+        -H 'sec-ch-ua: "Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"' \
+        -H 'sec-ch-ua-mobile: ?0' \
+        -H 'sec-ch-ua-platform: "Windows"' \
+        -H 'sec-fetch-dest: document' \
+        -H 'sec-fetch-mode: navigate' \
+        -H 'sec-fetch-site: none' \
+        -H 'sec-fetch-user: ?1' \
+        -H 'upgrade-insecure-requests: 1' \
+        -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
+        --compressed
     printf "%s, " "$distro"
 done
 
