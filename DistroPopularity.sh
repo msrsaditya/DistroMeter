524f8b2abb232cb27be3558aaf3ad3cad2d77a1c
Shashank Aditya <msrsaditya@gmail.com>
2025-05-17 22:53:38 +0530

    Update DistroPopularity.sh

--- DistroPopularity.sh
+++ DistroPopularity.sh
@@ -107,9 +107,46 @@ done
 
 printf "\n\nPopularity is as Follows:\n\n"
 
+counts=()
 for distro in "${distros[@]}"; do
     number=$(grep -m 1 '<faceplate-number number' "$distro.html" | awk -F'"' '{print $2}')
-    echo "$distro: $number"
-done | sort -t ':' -k2nr
+    counts+=("$number")
+done
+
+sorted=()
+for i in "${!distros[@]}"; do
+    sorted+=("${counts[i]}:::${distros[i]}")
+done
+
+IFS=$'\n'
+sorted=($(printf "%s\n" "${sorted[@]}" | sort -t: -k1,1nr))
+unset IFS
+
+sorted_distros=()
+sorted_counts=()
+for entry in "${sorted[@]}"; do
+    count=${entry%%::*}
+    distro=${entry##*:::}
+    sorted_counts+=("$count")
+    sorted_distros+=("$distro")
+done
+
+printf "distributions=["
+for i in "${!sorted_distros[@]}"; do
+    printf "\"%s\"" "${sorted_distros[i]}"
+    if [ $i -lt $(( ${#sorted_distros[@]} - 1 )) ]; then
+        printf ", "
+    fi
+done
+printf "]\n"
+
+printf "popularity=["
+for i in "${!sorted_counts[@]}"; do
+    printf "%s" "${sorted_counts[i]}"
+    if [ $i -lt $(( ${#sorted_counts[@]} - 1 )) ]; then
+        printf ", "
+    fi
+done
+printf "]\n\n"
 
 rm -rf *.html
